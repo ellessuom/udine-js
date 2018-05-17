6181ebb580aeeead691783c9bc52a279ff7bb31b
Ellessuom <demousselle42@gmail.com>
2018-05-18 00:49:11 +0200

    Some fixes in main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,25 @@
-let text = 'udine js';
-
-let len = 0;
-var writeSlogan = function () {
-    $('#cover-title').html(text.substr(0, len++));
+var len = 0;
+var write = function (target, text) {
+    target.html(text.substr(0, len++));
     if (len < text.length + 1) {
-      let time = Math.ceil(Math.random()*200)+10
-      setTimeout('writeSlogan()', time);
+      var time = Math.ceil( Math.random()*200 ) + 10;
+      setTimeout(function () { write(target, text); }, time);
     } else {
       len = 0;
       text = '';
     }
-  }
-
-var init = function () {
-    setTimeout(writeSlogan, 1000);
 };
 
-// document.addEventListener("DOMContentLoaded", function(event) { 
-//     init();
-// });
+var writeSlogan = function () {
+    write($('#cover-title'), 'udine js');
+};
+
+var init = function () {
+    setTimeout(function() {
+        writeSlogan();
+    }, 1000);
+};
 
 window.onload = (function (){
-    init()
+    init();
 });
