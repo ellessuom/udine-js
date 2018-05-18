b3eb16f816caa6934f8c8576fbf148c247ea6a0b
Ellessuom <demousselle42@gmail.com>
2018-05-18 22:07:40 +0200

    Added down icon fade-in animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,27 @@
 var len = 0;
-var write = function (target, text) {
+var write = function (target, text, cb) {
     target.html(text.substr(0, len++));
     if (len < text.length + 1) {
       var time = Math.ceil( Math.random()*200 ) + 10;
-      setTimeout(function () { write(target, text); }, time);
+      setTimeout(function () { write(target, text, cb); }, time);
     } else {
-      len = 0;
-      text = '';
+      cb();
     }
 };
 
+
 var writeSlogan = function () {
-    write($('#cover-title'), 'udine js');
+    write($('#cover-title'), 'udine js', function () {
+        setTimeout(function () {
+            $('#down-icon').fadeIn();
+        }, 500);
+    });
 };
 
 var init = function () {
     setTimeout(function() {
         writeSlogan();
-    }, 1000);
+    }, 750);
 };
 
 window.onload = (function (){
