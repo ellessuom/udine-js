720fba5253eaad2b6e2a371bac7dfb3227a7124e
Ellessuom <demousselle42@gmail.com>
2018-05-24 20:17:38 +0200

    Removed some comments

--- src/index.js
+++ src/index.js
@@ -26,21 +26,3 @@ window.onload = (function (){
     init();
     addSocialsListeners();
 });
-
-
-// function openSocial (social_name) {
-    
-//     let win;
-//     switch (social_name) {
-//         case 'twitter':
-//             win = window.open('https://twitter.com/Udine_JS', '_blank');
-//             break;
-//         case 'instagram':
-//             win = window.open('https://www.instagram.com/udinejs', '_blank');
-//             break;
-//         case 'facebook':
-//             win = window.open('https://www.facebook.com/udinejs', '_blank');
-//             break;
-//     }
-//     win.focus();
-// };
