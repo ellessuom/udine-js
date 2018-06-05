1c56c5820aeb153d9acbdfeb9d76622beef64754
Ellessuom <demousselle42@gmail.com>
2018-06-05 23:09:46 +0200

    Added JS logo svg animation. Hiddend news section.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,55 @@
-const $  = require('jquery');
+const $ = require('jquery');
+const anime = require('animejs');
+
 const writer  = require('./scripts/writer');
 const socials = require('./data/socials');
 
+const runLogoAnimation = () => {
+    const logo_square = anime.timeline({
+        autoplay: true
+    });
+
+    logo_square
+    .add({
+        targets         : '.ico path.square',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing          : 'easeInOutSine',
+        duration        : 1200
+    }).add({
+        targets : '.ico path.square',
+        fill    : '#f0dc4f',
+        easing  : 'easeInOutSine',
+        duration: 800
+    });
+
+    const logo_j = anime({
+        targets         : '.ico path.j',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing          : 'easeInOutSine',
+        duration        : 600
+    });
+
+    const logo_s = anime({
+        delay           : 600,
+        targets         : '.ico path.s',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing          : 'easeInOutSine',
+        duration        : 600,
+    });
+};
+
 const init = () => {
-    setTimeout(function() {
+    runLogoAnimation();
+
+    setTimeout(function () {
         writer.titles();
     }, 500);
 
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         if ($(this).scrollTop() >= 100) {
-            $('footer').css({ 'transform': 'translateY(0px)' });
+            $('footer').css({
+                'transform': 'translateY(0px)'
+            });
         }
     });
 };
@@ -17,7 +57,7 @@ const init = () => {
 const addSocialsListeners = () => {
     $('#socials-container').children().each((i, element) => {
         element.addEventListener('click', () => {
-            window.open(socials[element.id].url, '_blank');    
+            window.open(socials[element.id].url, '_blank');
         }, false)
     });
 };
--- src/scripts/writer.js
+++ src/scripts/writer.js
@@ -4,8 +4,9 @@ let   len   = 0;
 const write = (target, text, cb) => {
     target.html(text.substr(0, len++));
     if (len < text.length + 1) {
-      let time = Math.ceil( Math.random()*200 ) + 10;
-      setTimeout(function () { write(target, text, cb); }, time);
+        setTimeout(() => { 
+            write(target, text, cb);
+        }, Math.ceil( Math.random()*200 ) + 10);
     } else {
       cb();
     }
